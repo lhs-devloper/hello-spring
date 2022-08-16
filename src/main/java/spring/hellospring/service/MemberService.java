81a30b1fa158f8600002343c33a55583967162d4
lhs-devloper <sdsd0114@naver.com>
2022-08-16 21:52:16 +0900

    MemberService(비지니스 로직 관점), Dependecn Injection(의존성주입)

---
+++ src/main/java/spring/hellospring/service/MemberService.java
@@ -0,0 +1,39 @@
+package spring.hellospring.service;
+
+import spring.hellospring.Repository.MemberRepository;
+import spring.hellospring.Repository.MemoryMemberRepository;
+import spring.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+        validatedDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validatedDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
